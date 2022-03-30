51ec6ed41968006f5fba56eaec8fd214e31225d8
Gulam Ali <gulam2275@gmail.com>
2022-03-30 10:31:48 +0530

    Initial Commit

---
+++ MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by Gulam Ali on 30/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
